977b37c372438577cc10ae0c006a564b1a5b8e61
haitug08 <haitug08@gmail.com>
2025-01-22 00:16:50 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -169,30 +169,7 @@ function customCalculate() {
   window.onload = setEndOfMonth;
 
 
-//要素を取得
-const modal = document.querySelector('.js-modal'),
-      open = document.querySelector('.js-modal-open'),
-      close = document.querySelector('.js-modal-close');
 
-//「開くボタン」をクリックしてモーダルを開く
-function modalOpen() {
-  modal.classList.add('is-active');
-}
-open.addEventListener('click', modalOpen);
-
-//「閉じるボタン」をクリックしてモーダルを閉じる
-function modalClose() {
-  modal.classList.remove('is-active');
-}
-close.addEventListener('click', modalClose);
-
-//「モーダルの外側」をクリックしてモーダルを閉じる
-function modalOut(e) {
-  if (e.target == modal) {
-    modal.classList.remove('is-active');
-  }
-}
-addEventListener('click', modalOut);
 
 const modal = document.querySelector('.js-modal');
 const open = document.querySelector('.js-modal-open');
