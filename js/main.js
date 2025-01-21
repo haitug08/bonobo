1f500799cf80a2af552ff4cd7a98d08da67cf98d
haitug08 <haitug08@gmail.com>
2025-01-21 23:31:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -167,3 +167,29 @@ function customCalculate() {
 
   // ページ読み込み後に月末日を設定
   window.onload = setEndOfMonth;
+
+
+//要素を取得
+const modal = document.querySelector('.js-modal'),
+      open = document.querySelector('.js-modal-open'),
+      close = document.querySelector('.js-modal-close');
+
+//「開くボタン」をクリックしてモーダルを開く
+function modalOpen() {
+  modal.classList.add('is-active');
+}
+open.addEventListener('click', modalOpen);
+
+//「閉じるボタン」をクリックしてモーダルを閉じる
+function modalClose() {
+  modal.classList.remove('is-active');
+}
+close.addEventListener('click', modalClose);
+
+//「モーダルの外側」をクリックしてモーダルを閉じる
+function modalOut(e) {
+  if (e.target == modal) {
+    modal.classList.remove('is-active');
+  }
+}
+addEventListener('click', modalOut);
