55e25aac92f9e8161a6302cd01b2683fb7fe12f1
haitug08 <haitug08@gmail.com>
2025-03-24 16:51:56 +0900

    Update anti-social.js

--- js/anti-social.js
+++ js/anti-social.js
@@ -19,3 +19,9 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function() {
+    $("#check_button").click(function() {
+        $("#result_url").show(); // URLの表示
+    });
+});
